1a87b7d97ac2ec634263f24365eef2b1192e3c0a
RafalStaszel <rafalstaszel@gmail.com>
2017-05-13 22:55:22 +0200

    dodane metody find, setavaible etc

--- src/main/java/com/kaos/controller/VideoController.java
+++ src/main/java/com/kaos/controller/VideoController.java
@@ -2,7 +2,9 @@ package com.kaos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,11 +33,27 @@ public class VideoController {
 	public @ResponseBody Iterable<Film> getAllFilms() {
 		return filmRepository.findAll();
 	}
-	@RequestMapping("/hello")
-    @ResponseBody
-    public String answer() {
-        return "Yes, server - ON";
-    }
-	
-	
+
+	@GetMapping("/findTitle/{title}")
+	public @ResponseBody Film findByTitle(@PathVariable String title) {
+		return filmRepository.findByTitleIgnoreCase(title);
+	}
+
+	@GetMapping("/findId/{id}")
+	public @ResponseBody Film findById(@PathVariable int id) {
+		return filmRepository.findById(id);
+	}
+
+	@GetMapping("/avaible/{avaible}/{id}")
+	public @ResponseBody String setAvaible(@PathVariable boolean avaible, @PathVariable int id) {
+		filmRepository.findById(id).setAvaible(avaible);
+		return "Movie " + filmRepository.findById(id).getTitle() + " set avaible " + avaible;
+
+	}
+
+	@GetMapping("/test")
+	public @ResponseBody String answer() {
+		return "Yes, server - ON";
+	}
+
 }
--- src/main/java/com/kaos/repository/FilmRepository.java
+++ src/main/java/com/kaos/repository/FilmRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import com.kaos.video.model.Film;
 
 public interface FilmRepository extends CrudRepository<Film, Integer> {
-	// Film findByTitle(String title);
+	 Film findByTitleIgnoreCase(String title);
+	 Film findById(int id);
+	 Film removeByTitleIgnoreCase(String title);
+	;
 
 }
